a14d323e5bf48a6f32737f7feba373e55fb0180a
Suryakanta Prusty <112789661+Suraj-0210@users.noreply.github.com>
2024-03-22 15:08:41 +0000

    DDSF

--- database.py
+++ database.py
@@ -28,9 +28,7 @@ def load_jobs_from_db_by_id(id):
 
 def store_application_in_db(job_id, data):
   with engine.connect() as conn:
-    query = text(f"INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES ({job_id}, '{data['full_name']}', '{data['email']}', '{data['linkedin_url']}', '{data['education']}', '{data['work_experience']}', '{data['resume_url']}');")
-    QUERY1 = text("insert into applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) values (3, 'sURYAKANTA', 'kantaprustys@gmail.com', 'www.linkedin.com/in/suryakanta-%%E0%%A5%%A4%%E0%%A5%%A4-aa1542227', 'fewaf', 'faew', 'www.suraj-url.com');")
-    conn.execute(QUERY1)
+    conn.execute(text(f"INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES ({job_id}, '{data['full_name']}', '{data['email']}', '{data['linkedin_url']}', '{data['education']}', '{data['work_experience']}', '{data['resume_url']}')"))
 
 # query = text(f"INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
 # conn.execute(query, 
@@ -42,4 +40,3 @@ def store_application_in_db(job_id, data):
 #             work_experience=data['work_experience'],
 #             resume_url=data['resume_url'])
 
-# insert into applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) values (3, 'sdad', 'kantaprustys@gmail.com', 'www.linkedin.com/in/suryakanta-%%E0%%A5%%A4%%E0%%A5%%A4-aa1542227', 'fewaf', 'faew', 'www.suraj-url.com');
